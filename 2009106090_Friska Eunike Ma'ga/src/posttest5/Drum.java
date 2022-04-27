89064c878b59e2fe64cb824caa8ac1aca9398996
Be-Frisk <beefriska@gmail.com>
2022-04-27 21:48:51 +0800

    posttest 5

---
+++ 2009106090_Friska Eunike Ma'ga/src/posttest5/Drum.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posttest5;
+
+import posttest4.*;
+
+/**
+ *
+ * @author acer
+ */
+public final class Drum extends AlatMusik {
+    private final String jenis = "Drum";
+    private String bahan;
+    private int jumlahKit;
+    
+    public Drum(String tipe,int harga,String bahan,int jumlahKit){
+        super(tipe,harga);
+        this.bahan = bahan;
+        this.jumlahKit = jumlahKit;
+    }    
+
+    @Override
+    public void tampilkanData(){
+//        super.tampilkanData();
+        System.out.println("Bahan : " + this.bahan);
+        System.out.println("Jumlah Kit : " + this.jumlahKit +"\n");
+    }    
+    
+    public final void display(){
+       System.out.println("Jenis : " + this.jenis);
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+    
+    public String getBahan() {
+        return bahan;
+    }
+
+    public void setBahan(String bahan) {
+        this.bahan = bahan;
+    }
+
+    public int getJumlahKit() {
+        return jumlahKit;
+    }
+
+    public void setJumlahKit(int jumlahKit) {
+        this.jumlahKit = jumlahKit;
+    }
+    
+}
---
+++ 2009106090_Friska Eunike Ma'ga/src/posttest5/Piano.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posttest5;
+
+import posttest4.*;
+
+/**
+ *
+ * @author acer
+ */
+public class Piano extends AlatMusik {
+    private final String jenis = "Piano";
+    private int jumlahTuts;
+    private String bahanTuts;
+    
+    public Piano(String tipe,int harga,int jumlahTuts,String bahanTuts){
+        super(tipe,harga);
+        this.jumlahTuts = jumlahTuts;
+        this.bahanTuts = bahanTuts;
+    }    
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    @Override
+    public void tampilkanData(){
+//        super.tampilkanData();
+        System.out.println("Bahan Tuts : " + this.bahanTuts);
+        System.out.println("Jumlah Tuts : " + this.jumlahTuts + "\n");
+    }
+    
+    public final void display(){
+       System.out.println("Jenis : " + this.jenis);
+    }
+    
+    public int getJumlahTuts() {
+        return jumlahTuts;
+    }
+
+    public void setJumlahTuts(int jumlahTuts) {
+        this.jumlahTuts = jumlahTuts;
+    }
+
+    public String getBahanTuts() {
+        return bahanTuts;
+    }
+
+    public void setBahanTuts(String bahanTuts) {
+        this.bahanTuts = bahanTuts;
+    }
+
+}  
+
+
